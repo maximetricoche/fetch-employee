842741b5f92669fac471848f00f6c6a15d13c3ea
Maxime TRICOCHE <maxime.trche@gmail.com>
2024-10-30 11:51:20 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+
+function App() {
+	const [employee, setEmployee] = useState(sampleEmployee);
+
+	const getEmployee = () => {
+		// Send the request
+		fetch("https://randomuser.me/api?nat=en")
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+
+				setEmployee(data.results[0]);
+			});
+	};
+
+	return (
+		<div className="App">
+			<EmployeeCard employee={employee} />
+			<button type="button" onClick={getEmployee}>
+				Get employee
+			</button>
+		</div>
+	);
+}
+
+const sampleEmployee = {
+	name: {
+		first: "Charlie",
+		last: "Thompson",
+	},
+	email: "charlie.thompson@example.com",
+	picture: {
+		medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+	},
+};
+
+export default App;
